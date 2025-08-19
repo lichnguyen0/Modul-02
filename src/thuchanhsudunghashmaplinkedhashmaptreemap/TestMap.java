f55b0db02849a98813c2b438c04ea7c0af1c6a0f
lichnguyen0 <fakesangmy@gmail.com>
2025-08-19 11:34:59 +0700

    update

--- src/thuchanhsudunghashmaplinkedhashmaptreemap/TestMap.java
+++ src/thuchanhsudunghashmaplinkedhashmaptreemap/TestMap.java
@@ -1,4 +1,34 @@
 package thuchanhsudunghashmaplinkedhashmaptreemap;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TestMap {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> hashmap = new HashMap<>();
+
+        hashmap.put("Smith", 30);
+        hashmap.put("Anderson", 31);
+        hashmap.put("Lewis", 29);
+        hashmap.put("Cook", 29);
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashmap + "\n");
+
+        Map<String, Integer> treeMap = new TreeMap<>(hashmap);
+        System.out.println("Display entries in ascending order of key");
+        System.out.println(treeMap);
+
+
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>(16, 0.75f, true);
+
+        linkedHashMap.put("Smith", 30);
+        linkedHashMap.put("Anderson", 31);
+        linkedHashMap.put("Lewis", 29);
+        linkedHashMap.put("Cook", 29);
+        System.out.println("\nThe age  for" + " Lewis is " + linkedHashMap.get("Lewis"));
+    }
 }
