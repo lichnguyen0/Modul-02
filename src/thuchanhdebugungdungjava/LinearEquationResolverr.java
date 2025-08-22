4864b7b8e29b0b38bbcc8bd8c107e1f8c5cfa466
lichnguyen0 <fakesangmy@gmail.com>
2025-08-22 13:59:11 +0700

    update

---
+++ src/thuchanhdebugungdungjava/LinearEquationResolverr.java
@@ -0,0 +1,31 @@
+package thuchanhdebugungdungjava;
+
+import java.util.Scanner;
+
+public class LinearEquationResolverr {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+
+        if (a != 0) {
+            double solution = -b / a;
+            System.out.printf("The solution is: x = %f\n", solution);
+        } else {
+            if (b == 0) {
+                System.out.println("The solution is all x!");
+            } else {
+                System.out.println("No solution!");
+            }
+        }
+
+        scanner.close();
+    }
+}
